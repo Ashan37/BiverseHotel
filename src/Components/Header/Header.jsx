5405839aa0c6788f86111fb6eba063ca6026d119
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-02-27 16:42:47 +0530

    add navbar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'remixicon/fonts/remixicon.css';
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" bg='dark' data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="#"><i class="ri-hotel-line"></i>BiverseHotel</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About US</Nav.Link>
+            <Nav.Link href="#action3">Contacts</Nav.Link>
+
+            <NavDropdown title="Show" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Dining</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Wedding & Events</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Offers</NavDropdown.Item>
+            </NavDropdown>
+  
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
