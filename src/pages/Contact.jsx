a035aa8cf2a7b6025946c244274ab1e02bb952f2
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-04 23:58:25 +0530

    Add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from '../Components/Header/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import Footer from '../Components/Footer/Footer';
+
 
 export default function Contact() {
   return (
     <div>
-      
+      <Container-fluid>
+        <Header />
+      </Container-fluid>
+
+         <Container-fluid>
+              <Footer />
+            </Container-fluid>
+
     </div>
   )
 }
