b0ebcb85083fa99f9b5dee070f2915e67855d822
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-07 22:42:24 +0530

    add dining page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
+import Dining from './pages/Dining'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/home' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/dining' element={<Dining/>}/>
       </Routes>
       </BrowserRouter>
     </>
