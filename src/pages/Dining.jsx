3a6a29615d77c988b94b1866fc6308b2e6f4ae6b
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-10 23:01:15 +0530

    update dining page

--- src/pages/Dining.jsx
+++ src/pages/Dining.jsx
@@ -3,6 +3,7 @@ import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 import { Container, Row, Col } from "react-bootstrap";
 import DiningHero from "../Components/heroslider/DiningHero";
+import "../styles/dining.css";
 
 export default function Dining() {
   return (
@@ -17,7 +18,9 @@ export default function Dining() {
       <Container className="content">
         <Row className="px-5">
           <Col lg="12" md="12" sm="12" className=" px-5">
-            <h1 className="dining-head d-flex justify-content-center">Dining At Our Place</h1>
+            <h1 className="dining-head d-flex justify-content-center">
+              Dining At Our Place
+            </h1>
             <p className="para">
               Indulge in a delightful culinary experience with our diverse
               dining options, where flavors from around the world meet fresh,
@@ -34,6 +37,36 @@ export default function Dining() {
           </Col>
         </Row>
       </Container>
+      <Container>
+      <Row className="g-3"> {/* Adds spacing between columns */}
+        {
+        [
+          {src:"src/assets/dining/inhotel.jpg", name:"Tango Hotel"},
+          {src:"src/assets/dining/coffee.jpg", name:"Coffee Shop"},
+          {src:"src/assets/dining/bar.jpg", name:"Hest  Bar"}
+          
+        ]
+        .map((place, index) => ( // Adjust number of images
+          <Col key={index} className="px-2" lg="4" md="12" sm="12" xs="12">
+            <div className="content_top">
+              <div className="content_top_photo d-flex justify-content-center">
+                <img 
+                  src={place.src}
+                  className="img-fluid rounded" 
+                  alt={place.name}
+                  style={{height:"600px",}}
+                 
+                />
+              </div>
+              <div className="content_top_description d-flex justify-content-center">
+                <h1 style={{fontSize:"1.5rem"}}>{place.name}</h1>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+
       <Container-fluid>
         <Footer />
       </Container-fluid>
