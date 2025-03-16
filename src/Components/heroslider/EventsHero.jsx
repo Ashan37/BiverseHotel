1bb4476f6c6e482f0821f9c90b2bf2eace68b5cf
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-16 21:30:13 +0530

    add hero section to events page

---
+++ src/Components/heroslider/EventsHero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '/src/styles/eventshero.css'
+
+export default function EventsHero() {
+  return (
+    <div className="herosection">
+  </div>
+
+  )
+}
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
+import EventsHero from '../Components/heroslider/EventsHero'
 
 export default function Events() {
   return (
@@ -10,7 +11,7 @@ export default function Events() {
       </Container-fluid>
 
       <Container-fluid>
-        
+        <EventsHero />
       </Container-fluid>
       <Container-fluid>
               <Footer />
